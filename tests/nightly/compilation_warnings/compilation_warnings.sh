7400f95dc6375e325f0453d92b85ca208d55da6f
Rahul <rahulhuilgol@gmail.com>
2017-07-25 14:54:59 -0700

    Update compilation_warnings.sh

--- tests/nightly/compilation_warnings/compilation_warnings.sh
+++ tests/nightly/compilation_warnings/compilation_warnings.sh
@@ -11,19 +11,14 @@ runme() {
 		exit 1
 	fi
 }
-g++ -dumpversion
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get -y install time g++-5
-sudo update-alternatives --remove-all gcc 
-sudo update-alternatives --remove-all g++
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-g++ -dumpversion
-
 runme make clean >/dev/null
 runme mkdir build
 echo "Starting make"
-runme /usr/bin/time -f "%e" make -j$(nproc) &> build/compile_output.txt
+runme CC="g++-5" CXX="g++-5" /usr/bin/time -f "%e" make -j$(nproc) &> build/compile_output.txt
+head -10 build/compile_output.txt
 echo "Finished make. Now processing output"
 python tests/nightly/compilation_warnings/process_output.py build/compile_output.txt
