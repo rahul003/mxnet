26cabd56864e8e8a74f7ba9a15422d188132aecf
Rahul Huilgol <rahulhuilgol@gmail.com>
2017-08-31 15:16:41 -0700

    Update compilation_warnings.sh

--- tests/nightly/compilation_warnings/compilation_warnings.sh
+++ tests/nightly/compilation_warnings/compilation_warnings.sh
@@ -37,10 +37,10 @@ sudo apt-get -y install time g++-5
 runme make clean >/dev/null
 runme mkdir build
 echo "Starting make on CPU with g++5"
-runme /usr/bin/time -f "%e" env CC=gcc-5 CXX=g++-5 make USE_OPENCV=1 USE_BLAS=openblas -j $(nproc) 2>&1 | tee build/cpu_compile_log.txt
+runme /usr/bin/time -f "%e" make CC=gcc-5 CXX=g++-5 USE_OPENCV=1 USE_BLAS=openblas -j $(nproc) 2>&1 | tee build/cpu_compile_log.txt
 echo "##############################"
 echo "Starting make on GPU with g++5"
-runme /usr/bin/time -f "%e" env CC=gcc-5 CXX=g++-5 make USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 -j $(nproc) 2>&1 | tee build/gpu_compile_log.txt
+runme /usr/bin/time -f "%e" make CC=gcc-5 CXX=g++-5 USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 -j $(nproc) 2>&1 | tee build/gpu_compile_log.txt
 echo "Finished make. Now processing output"
 python tests/nightly/compilation_warnings/process_output.py cpu build/cpu_compile_output.txt
 python tests/nightly/compilation_warnings/process_output.py gpu build/gpu_compile_output.txt
