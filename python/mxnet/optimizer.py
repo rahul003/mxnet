a07c6d672eec462db9384d850a66aab0c9e5e5f3
Rahul Huilgol <rahulhuilgol@gmail.com>
2018-05-12 00:43:11 +0000

    fix lars

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -28,7 +28,7 @@ from .ndarray import (NDArray, zeros, clip, sqrt, cast, maximum, abs as NDabs)
 from .ndarray import (sgd_update, sgd_mom_update, adam_update, rmsprop_update, rmspropalex_update,
                       mp_sgd_update, mp_sgd_mom_update, square, ftrl_update, ftml_update,
                       signsgd_update, signum_update, larc_sgd_mom_update, larc_sgd_update, larc_mp_sgd_update, larc_mp_sgd_mom_update)
-from .ndarray import sparse
+from .ndarray import sparse, multiply
 from .random import normal
 
 
